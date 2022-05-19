1bb83d4a470e5e9ff24a012c746b1b6014e16ced
Nils Ratusznik <nils.github@anotherhomepage.org>
2022-05-19 18:30:03 +0200

    terraform: use Fedora 36 stable base image file

--- terraform/volumes.tf
+++ terraform/volumes.tf
@@ -1,7 +1,7 @@
 resource "libvirt_volume" "fedora_image" {
   name = "fedora_image.qcow2"
-  source = "/home/nils/libvirt/boot/Fedora-Cloud-Base-36_Beta-1.4.x86_64.qcow2"
-  #source = "https://ftp.lip6.fr/ftp/pub/linux/distributions/fedora/releases/test/36_Beta/Cloud/x86_64/images/Fedora-Cloud-Base-36_Beta-1.4.x86_64.qcow2"
+  source = "/home/nils/libvirt/boot/Fedora-Cloud-Base-36-1.5.x86_64.qcow2"
+  #source = "https://ftp.lip6.fr/ftp/pub/linux/distributions/fedora/releases/36/Cloud/x86_64/images/Fedora-Cloud-Base-36-1.5.x86_64.qcow2"
   pool = "nils_images"
 }
 
